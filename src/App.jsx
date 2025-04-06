8fda130809baff6635f2e25ac83a005d62824a19
Jason Tajor <jasontajor0@gmail.com>
2025-04-07 01:02:02 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import { createHashHistory } from 'history';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -13,8 +11,6 @@ import ScrollToTop from "./Utils/ScrollToTop";
 import Loader from "./Utils/Loader";
 import NotFound from "./components/NotFound";
 
-const hashHistory = createHashHistory({ hashType: "/" });
-
 const App = () => {
   const [loading, setLoading] = useState(true);
 
@@ -22,18 +18,14 @@ const App = () => {
     const delay = setTimeout(() => {
       setLoading(false);
     }, 3000);
-
     return () => clearTimeout(delay);
   }, []);
-
-  const connectionType =
-    navigator.connection && navigator.connection.effectiveType;
 
   return (
     <>
       {loading && <Loader />}
       {!loading && (
-        <Router history={hashHistory}>
+        <Router>
           <Routes>
             <Route
               path="/about"
@@ -46,7 +38,6 @@ const App = () => {
                 </>
               }
             />
-
             <Route
               path="/contact"
               element={
